8d784c2f27d3b44e48e3e968a90920dbe9c01d7f
Markus Hager <meks77@gmail.com>
2025-04-26 10:10:13 +0200

    PowerFileParser: optimized usage of csv-parser

--- src/main/java/at/meks/powerdataupload/PowerFileParser.java
+++ src/main/java/at/meks/powerdataupload/PowerFileParser.java
@@ -19,33 +19,32 @@ import java.util.stream.Stream;
 
 class PowerFileParser {
 
-    public static final String MESSZEITPUNKT = "Messzeitpunkt";
-    public static final String EINSPEISUNG_KWH = "Einspeisung (kWh)";
+    public static final String TIMESTAMP = "timestamp";
+    public static final String POWER = "power";
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm");
     public static final NumberFormat NUMBER_PARSER = NumberFormat.getNumberInstance(Locale.GERMAN);
-    public static final String VERBRAUCH_KWH = "Verbrauch (kWh)";
 
     private final Iterable<CSVRecord> fedInRecords;
     private final Iterable<CSVRecord> consumptionRecords;
 
     PowerFileParser(Reader fedInReader, Reader consumptionReader) throws IOException {
-        fedInRecords = CSVFormat.Builder.create()
+        fedInRecords = CSVFormat.RFC4180.builder()
                                         .setDelimiter(";")
-                                        .setHeader(MESSZEITPUNKT, EINSPEISUNG_KWH)
+                                        .setHeader(TIMESTAMP, POWER)
+                                        .setSkipHeaderRecord(true)
                                         .get()
                                         .parse(fedInReader);
-        consumptionRecords = CSVFormat.Builder.create()
-                .setDelimiter(";")
-                .setHeader(MESSZEITPUNKT, VERBRAUCH_KWH)
-                .get()
-                .parse(consumptionReader);
+        consumptionRecords = CSVFormat.RFC4180.builder()
+                                              .setDelimiter(";")
+                                              .setHeader(TIMESTAMP, POWER)
+                                              .setSkipHeaderRecord(true)
+                                              .get()
+                                              .parse(consumptionReader);
     }
 
     Stream<PowerData> stream() {
         Iterator<CSVRecord> fedInIterator = fedInRecords.iterator();
         Iterator<CSVRecord> consumptionIterator = consumptionRecords.iterator();
-        fedInIterator.next();
-        consumptionIterator.next();
 
         Optional<CSVRecord> currentFedInRecord = Optional.of(fedInIterator.next());
         Optional<CSVRecord> currentConsumptionRecord = Optional.of(consumptionIterator.next());
@@ -59,11 +58,13 @@ class PowerFileParser {
                 powerData.add(getPowerData(currentFedInRecord.get(), currentConsumptionRecord.get()));
                 currentFedInRecord = nextRecord(fedInIterator);
                 currentConsumptionRecord = nextRecord(consumptionIterator);
-            } else if (fedInIsBeforeConsumption(currentFedInTimestamp, currentConsumptionTimestamp) || onlyFedInIsAvailable(currentFedInRecord, currentConsumptionRecord)) {
+            } else if (fedInIsBeforeConsumption(currentFedInTimestamp, currentConsumptionTimestamp)
+                    || onlyFedInIsAvailable(currentFedInRecord, currentConsumptionRecord)) {
                 //noinspection OptionalGetWithoutIsPresent -> it is verified in the if condition in the method call
                 powerData.add(getPowerDataForFedInRecord(currentFedInRecord.get()));
                 currentFedInRecord = nextRecord(fedInIterator);
-            } else if (consumptionIsBeforeFedIn(currentFedInTimestamp, currentConsumptionTimestamp) || onlyConsumptionIsAvailable(currentFedInRecord, currentConsumptionRecord)) {
+            } else if (consumptionIsBeforeFedIn(currentFedInTimestamp, currentConsumptionTimestamp)
+                    || onlyConsumptionIsAvailable(currentFedInRecord, currentConsumptionRecord)) {
                 //noinspection OptionalGetWithoutIsPresent -> it is verified in the if condition in the method call
                 powerData.add(getPowerDataForConsumptionRecord(currentConsumptionRecord.get()));
                 currentConsumptionRecord = nextRecord(consumptionIterator);
@@ -83,20 +84,20 @@ class PowerFileParser {
 
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     private static boolean onlyConsumptionIsAvailable(Optional<CSVRecord> currentFedInRecord,
-                                      Optional<CSVRecord> currentConsumptionRecord) {
+                                                      Optional<CSVRecord> currentConsumptionRecord) {
         return currentFedInRecord.isEmpty() && currentConsumptionRecord.isPresent();
     }
 
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     private static boolean consumptionIsBeforeFedIn(Optional<LocalDateTime> currentFedInTimestamp,
-                                      Optional<LocalDateTime> currentConsumptionTimestamp) {
+                                                    Optional<LocalDateTime> currentConsumptionTimestamp) {
         return currentFedInTimestamp.isPresent() && currentConsumptionTimestamp.isPresent()
                 && currentFedInTimestamp.get().isAfter(currentConsumptionTimestamp.get());
     }
 
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     private static boolean fedInIsBeforeConsumption(Optional<LocalDateTime> currentFedInTimestamp,
-                                      Optional<LocalDateTime> currentConsumptionTimestamp) {
+                                                    Optional<LocalDateTime> currentConsumptionTimestamp) {
         return currentFedInTimestamp.isPresent() && currentConsumptionTimestamp.isPresent()
                 && currentFedInTimestamp.get().isBefore(currentConsumptionTimestamp.get());
     }
@@ -110,37 +111,39 @@ class PowerFileParser {
     }
 
     private static PowerData getPowerData(CSVRecord fedInRecord, CSVRecord consumptionRecord) {
-        String fedInTimestamp = fedInRecord.get(MESSZEITPUNKT);
-        String consumptionTimestamp = consumptionRecord.get(MESSZEITPUNKT);
+        String fedInTimestamp = fedInRecord.get(TIMESTAMP);
+        String consumptionTimestamp = consumptionRecord.get(TIMESTAMP);
         if (!fedInTimestamp.equals(consumptionTimestamp)) {
-            throw new IllegalStateException("Fed-In timestamp '" + fedInTimestamp + "' and consumption timestamp '" + consumptionTimestamp + "' don't match");
+            throw new IllegalStateException(
+                    "Fed-In timestamp '" + fedInTimestamp + "' and consumption timestamp '" + consumptionTimestamp
+                            + "' don't match");
         }
         return new PowerData(getTimestamp(fedInRecord),
-                             getPowerKwh(fedInRecord, EINSPEISUNG_KWH),
-                             getPowerKwh(consumptionRecord, VERBRAUCH_KWH));
+                             getPowerKwh(fedInRecord),
+                             getPowerKwh(consumptionRecord));
     }
 
     private static PowerData getPowerDataForFedInRecord(CSVRecord fedInRecord) {
         return new PowerData(getTimestamp(fedInRecord),
-                             getPowerKwh(fedInRecord, EINSPEISUNG_KWH),
+                             getPowerKwh(fedInRecord),
                              0.0);
     }
 
     private static PowerData getPowerDataForConsumptionRecord(CSVRecord consumptionRecord) {
         return new PowerData(getTimestamp(consumptionRecord),
                              0.0,
-                             getPowerKwh(consumptionRecord, VERBRAUCH_KWH));
+                             getPowerKwh(consumptionRecord));
     }
 
-    private static double getPowerKwh(CSVRecord record, String columnHeader) {
+    private static double getPowerKwh(CSVRecord record) {
         try {
-            return NUMBER_PARSER.parse(record.get(columnHeader)).doubleValue();
+            return NUMBER_PARSER.parse(record.get(PowerFileParser.POWER)).doubleValue();
         } catch (ParseException e) {
-            throw new IllegalStateException(columnHeader + " couldn't be parsed", e);
+            throw new IllegalStateException(PowerFileParser.POWER + " couldn't be parsed", e);
         }
     }
 
     private static LocalDateTime getTimestamp(CSVRecord record) {
-        return LocalDateTime.parse(record.get(MESSZEITPUNKT), DATE_TIME_FORMATTER);
+        return LocalDateTime.parse(record.get(TIMESTAMP), DATE_TIME_FORMATTER);
     }
 }
